bcc36614dbe051fd4b6820696a1f4e9cc642cc7d
Moucharraf <moucharaf667@gmail.com>
2022-03-25 07:01:44 +0100

    connexion reussie

--- SignUp.php
+++ SignUp.php
@@ -1,15 +1,17 @@
 <?php
-$servername = 'localhost';
-$username = 'root';
-$password = '1075';
-$base ='client';
-$con= mysqli_connect($servername, $username, $password, $base);
+session_start();
 
-if(!$con){
-    die("Echec de la connexion :" . mysqli_connect_error());
+try{
+    $con= new PDO('mysql:host=localhost;dbname=agile;charset=utf8;port=3307','root','root');
+    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo 'connexion reussie';
 }
-echo "Connexion reussie";
 
+catch(PDOException $e){
+    echo "Echec de la connexion :" . $e->getMessage();
+}
+
+/*
 $nom = $_POST['nomUtilisateur'];
 $prenom = $_POST['prenomUtilisateur'];
 $date = $_POST['date_n'];
@@ -17,13 +19,11 @@ $pays = $_POST['pays'];
 $email = $_POST['mail'];
 $password = $_POST['motDePasee'];
 $genre = $_POST['sexe'];
+*/
 
-$sql = "INSERT INTO client(Nom,Prenom,Date_Naissance,Pays,Email,MotDePasse,Sexe) VALUES($nom,$prenom,$date,$pays,$email,$password,$genre)";
-if(mysqli_query($con,$sql)){
-    echo "Compte cree!";
-    echo "BIENVENUE SUR NOTRE PLATEFORME";
-}else{
-    echo "Erreur : ". mysqli_error($con);
-}
-mysqli_close($con);
+$sql = "INSERT INTO client(IdClient,Nom,Prenom,Pays,Email,MotDePasse,Sexe) VALUES(1,'test','test1','tunisie','test@gmail.com','testt','masculin')";
+$requete = $con->prepare($sql);
+$requete->execute();
+$con=NULL;
+
 ?>
